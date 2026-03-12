09322e0476f773676038612cff129e7ed689e197
LeonardoZerbinattiDeSales <leonardozerbinattidesales@gmail.com>
2026-03-12 18:13:57 -0300

    refactor(domain): inicializa coleções de relacionamento na entidade produto

--- Loja/Loja.Domain/Entities/Produto.cs
+++ Loja/Loja.Domain/Entities/Produto.cs
@@ -3,14 +3,14 @@ namespace Loja.Domain.Entities;
 public class Produto
 {
     public int IdProduto { get; set; }
-    
+
     public string Nome { get; set; } = string.Empty;
-    
+
     public decimal Preco { get; set; }
     
-    public ICollection<ItemPedido>? ItensPedido { get; set; }
+    public ICollection<ItemPedido> ItensPedido { get; set; } = new List<ItemPedido>();
     
-    public ICollection<Estoque>? Estoques { get; set; }
+    public ICollection<Estoque> Estoques { get; set; } = new List<Estoque>();
     
-    public ICollection<Categoria>? Categorias { get; set; }
+    public ICollection<Categoria> Categorias { get; set; } = new List<Categoria>();
 }
