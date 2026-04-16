28c8397ad2809ad28bc7f90bac39dee6862863e2
justigliani <julianasilva.vania@gmail.com>
2026-04-16 16:27:42 -0300

    feat: adicionando mappings de Cliente e Endereco

--- Loja/Loja.Infrastructure/Mappings/ClienteMapping.cs
+++ Loja/Loja.Infrastructure/Mappings/ClienteMapping.cs
@@ -1,6 +1,46 @@
+using Loja.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace Loja.Infrastructure.Mappings;
 
-public class ClienteMapping
+public class ClienteMapping : IEntityTypeConfiguration<Cliente>
 {
-    
+    public void Configure(EntityTypeBuilder<Cliente> builder)
+    {
+        builder.ToTable("Clientes");
+        builder.HasKey(c => c.IdCliente);
+
+        builder.Property(c => c.Nome)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(c => c.Email)
+            .IsRequired()
+            .HasMaxLength(150);
+
+        builder.HasIndex(c => c.Email).IsUnique();
+
+        builder.Property(c => c.Senha)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(c => c.Telefone)
+            .HasMaxLength(20);
+
+        builder.Property(c => c.DataCadastro)
+            .IsRequired();
+
+        // 1:N -> Enderecos
+        builder.HasMany(c => c.Enderecos)
+            .WithOne(e => e.Cliente)
+            .HasForeignKey(e => e.ClienteId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // 1:N -> Pedidos
+        builder.HasMany(c => c.Pedidos)
+            .WithOne(p => p.Cliente)
+            .HasForeignKey(p => p.ClienteId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
--- Loja/Loja.Infrastructure/Mappings/EnderecoMapping.cs
+++ Loja/Loja.Infrastructure/Mappings/EnderecoMapping.cs
@@ -1,6 +1,18 @@
+using Loja.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace Loja.Infrastructure.Mappings;
 
-public class EnderecoMapping
+public class EnderecoMapping : IEntityTypeConfiguration<Endereco>
 {
-    
+    public void Configure(EntityTypeBuilder<Endereco> builder)
+    {
+        builder.ToTable("Enderecos");
+        builder.HasKey(e => e.IdEndereco);
+        builder.Property(e => e.Rua).IsRequired().HasMaxLength(200);
+        builder.Property(e => e.Cidade).IsRequired().HasMaxLength(100);
+        builder.Property(e => e.Estado).IsRequired().HasMaxLength(50);
+        builder.Property(e => e.Cep).IsRequired().HasMaxLength(10);
+    }
 }
