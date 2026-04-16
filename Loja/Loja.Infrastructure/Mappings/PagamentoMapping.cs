64da97b065c3c193e3639f06d0f028a5ee501827
justigliani <julianasilva.vania@gmail.com>
2026-04-16 16:30:45 -0300

    feat: adicionando mappings de Pedido e Pagamento

--- Loja/Loja.Infrastructure/Mappings/PagamentoMapping.cs
+++ Loja/Loja.Infrastructure/Mappings/PagamentoMapping.cs
@@ -1,6 +1,17 @@
+using Loja.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace Loja.Infrastructure.Mappings;
 
-public class PagamentoMapping
+public class PagamentoMapping : IEntityTypeConfiguration<Pagamento>
 {
-    
+    public void Configure(EntityTypeBuilder<Pagamento> builder)
+    {
+        builder.ToTable("Pagamentos");
+        builder.HasKey(p => p.IdPagamento);
+        builder.Property(p => p.MetodoPagamento).IsRequired().HasMaxLength(50);
+        builder.Property(p => p.Status).IsRequired().HasMaxLength(50);
+        builder.Property(p => p.DataPagamento).IsRequired();
+    }
 }
--- Loja/Loja.Infrastructure/Mappings/PedidoMapping.cs
+++ Loja/Loja.Infrastructure/Mappings/PedidoMapping.cs
@@ -1,6 +1,30 @@
+using Loja.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace Loja.Infrastructure.Mappings;
 
-public class PedidoMapping
+public class PedidoMapping : IEntityTypeConfiguration<Pedido>
 {
-    
+    public void Configure(EntityTypeBuilder<Pedido> builder)
+    {
+        builder.ToTable("Pedidos");
+        builder.HasKey(p => p.IdPedido);
+
+        builder.Property(p => p.DataPedido).IsRequired();
+
+        builder.Property(p => p.Status)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(p => p.ValorTotal)
+            .HasColumnType("decimal(10,2)");
+
+        // 1:1 -> Pagamento (opcional: pedido pode existir sem pagamento ainda)
+        builder.HasOne(p => p.Pagamento)
+            .WithOne(pg => pg.Pedido)
+            .HasForeignKey<Pagamento>(pg => pg.PedidoId)
+            .IsRequired(false)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
